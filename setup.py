f7514e828c3a1e4f99e285dca05bfdac6c1fae46
RamaneekGill <ramaneek.gill@hotmail.com>
2017-04-09 16:30:04 -0400

    Setup the project as a PIP installable package

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Setup details for PIP"""
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='DicomBatchLoader',
+      version='0.0.1',
+      description='DICOM Batch Loader',
+      author='Ramaneek Gill',
+      author_email='ramaneek.gill@hotmail.com',
+      url='https://github.com/RamaneekGill/Dicom-Batch-Loader',
+      download_url='https://github.com/RamaneekGill/Dicom-Batch-Loader/tarball/0.0.1',
+      license='MIT',
+      packages=find_packages())
